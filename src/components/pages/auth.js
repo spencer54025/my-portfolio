ddc8ba4e17604b0f9bd623281e1b475224312d1c
spencer54025 <spencervp54025@gmail.com>
2022-04-29 10:10:57 -0600

    added auth styles

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react'
+import Image from '../../../static/assets/images/auth/saber-image.jpg'
 
 export default class Auth extends Component {
     render() {
-        return <div>Auth</div>;
+        return( 
+        <div className='auth-page-wrapper'>
+            <div className="left-column"
+                    style={{
+                    backgroundImage: `url(${Image})`
+                }}
+            />
+            <div className="right-column">
+                <h1>Login component goes here</h1>
+            </div>
+        </div>
+        )
     }
 }
