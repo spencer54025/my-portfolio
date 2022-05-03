13ae42161429c70a894adecbafb6a372d8511810
spencer54025 <spencervp54025@gmail.com>
2022-05-03 11:52:20 -0600

    added portfolio items and styles

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
+import PortfolioSidebarList from '../portfolio/portfolio-side-list'
 
 export default class PortfolioManager extends Component {
-    constructor(props){
-        super(props)
+    constructor(){
+        super()
+
+        this.state = {
+            portfolioItems: []
+        }
+    }
+
+    getPortfolioItems() {
+    axios.get("https://spencervp.devcamp.space/portfolio/portfolio_items", { withCredentials: true })
+        .then(response => {
+            this.setState({
+                portfolioItems: [...response.data.portfolio_items]
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems()
     }
 
     render() {
         return(
-            <div></div>
+            <div className='manager-container'>
+                <div className="left-side">
+
+                </div>
+                <div className="right-side">
+                    <PortfolioSidebarList data={this.state.portfolioItems}/>
+                </div>
+            </div>
         )
     }
 }
