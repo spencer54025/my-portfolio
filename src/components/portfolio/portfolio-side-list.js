2bd397a0a79471b94b81678494470bf8a97c9431
spencer54025 <spencervp54025@gmail.com>
2022-05-03 12:58:28 -0600

    populated item list component and added styles

--- src/components/portfolio/portfolio-side-list.js
+++ src/components/portfolio/portfolio-side-list.js
@@ -3,18 +3,21 @@ import React from 'react'
 const PortfoliosidebarList = (props) => {
     const portfolioList = props.data.map(portfolioItem => {
         return (
-            <div>
-            <div>
+            <div
+             className='portfolio-item'
+             key={portfolioItem.id}
+             >
+            <div className='portfolio-img'>
                 <img src={portfolioItem.thumb_image_url} />
             </div>
-                <h1>{portfolioItem.name}</h1>
+                <h1 className='title'>{portfolioItem.name}</h1>
                 <h2>{portfolioItem.id}</h2>
             </div>
         )
     })
 
     return(
-        <div>{portfolioList}</div>
+        <div className='portfolio-sidebar-wrapper'>{portfolioList}</div>
     )
 }
 
